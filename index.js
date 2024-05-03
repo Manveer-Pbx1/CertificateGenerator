fa99af65baead71e3755fd4cdf247986ef4c8d3a
Manveer <manveer7saggu@gmail.com>
2024-05-03 20:18:18 +0530

    init files

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const PORT = 7000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
